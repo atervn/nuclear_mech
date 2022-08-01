ccc1614e57300fca0e23d7d08c1181f1d35bccdf
atervn <aapo.tervonen@protonmail.com>
2022-08-01 15:42:29 +0300

    A lot of changes

--- main.jl
+++ main.jl
@@ -3,12 +3,32 @@ using Statistics
 using LinearAlgebra
 using IterativeSolvers
 using SparseArrays
+using ProgressMeter
+# using PlotlyJS
 
 include("sphere_creation.jl")
 include("plotting.jl")
 include("geometric_functions.jl")
 include("calculate_forces.jl")
 include("misc_functions.jl")
+
+Base.@kwdef mutable struct forcesType
+    volumeX::Vector{Float64} = []
+    volumeY::Vector{Float64} = []
+    volumeZ::Vector{Float64} = []
+    areaX::Vector{Float64} = []
+    areaY::Vector{Float64} = []
+    areaZ::Vector{Float64} = []
+    bendingX::Vector{Float64} = []
+    bendingY::Vector{Float64} = []
+    bendingZ::Vector{Float64} = []
+    elasticX::Vector{Float64} = []
+    elasticY::Vector{Float64} = []
+    elasticZ::Vector{Float64} = []
+    repulsionX::Vector{Float64} = []
+    repulsionY::Vector{Float64} = []
+    repulsionZ::Vector{Float64} = []
+end
 
 Base.@kwdef mutable struct nucleusType
     x::Vector{Float64} = []
@@ -31,73 +51,126 @@ Base.@kwdef mutable struct nucleusType
     normalVolume::Float64 = 0
     normalArea::Float64 = 0
     normalAngle::Float64 = 0
-    normalLength::Float64 = 0
+    normalLengths::Vector{Float64} = []
+    forces = forcesType()
 end
 
-Base.@kwdef mutable struct forcesType
-    volume::Array{Float64} = Array{Int64}[]
-    area::Array{Float64} = Array{Int64}[]
-    bending::Array{Float64} = Array{Int64}[]
-    elastic::Array{Float64} = Array{Int64}[]
-    repulsion::Array{Float64} = Array{Int64}[]
+Base.@kwdef mutable struct parametersType
+    bulkModulus::Float64 = 0;
+    areaCompressionStiffness::Float64 = 0;
+    bendingStiffness::Float64 = 0;
+    laminaStiffness::Float64 = 0;
+    repulsionConstant::Float64 = 0;
+    repulsionDistance::Float64 = 0;
+    laminaFriction::Float64 = 0;
+    viscosity::Float64 = 0;
 end
 
-Base.@kwdef struct parametersType
-    bulkModulus::Float64
-    areaCompressionStiffness::Float64
+Base.@kwdef mutable struct scaledParametersType
+    
+    bulkModulus::Float64 = 0;
+    areaCompressionStiffness::Float64 = 0;
+    bendingStiffness::Float64 = 0;
+    laminaStiffness::Float64 = 0;
+    repulsionConstant::Float64 = 0;
+    repulsionDistance::Float64 = 0;
+    laminaFriction::Float64 = 0;
+    viscosity::Float64 = 0;
 end
 
-nucleus = nucleusType();
-forces = forcesType();
+nuc = nucleusType();
+
+par = parametersType();
+
+ipar = read_parameters("./parameters.txt");
+
+mpar = calculate_model_parameters(ipar);
 
 radius = 1;
-nSubdivisions = 1;
+nSubdivisions = 3;
 
-nucleus = create_icosahedron!(nucleus,radius);
-nucleus = get_edges!(nucleus);
-nucleus = subdivide_mesh!(nucleus,radius,nSubdivisions)
-get_triangle_normals!(nucleus);
+nuc = create_icosahedron!(nuc,radius);
+nuc = get_edges!(nuc);
+nuc = subdivide_mesh!(nuc,radius,nSubdivisions)
+get_triangle_normals!(nuc);
 
-@time begin
-nucleus.normalVolume = get_volume!(nucleus);
-nucleus.normalArea = get_area!(nucleus);
-nucleus.normalAngle = mean(get_triangle_angles(nucleus));
-lengths = zeros(Float64,Int64(size(nucleus.edges,1)/2),1);
 
-local j = 1;
-for i = 1:size(nucleus.edges,1)
-    if nucleus.firstEdges[i] == 1
+nuc.normalVolume = get_volume!(nuc);
+nuc.normalArea = get_area!(nuc);
+nuc.normalAngle = mean(get_triangle_angles(nuc));
+lengths = zeros(Float64,Int64(size(nuc.edges,1)));
 
-        vector = [nucleus.x[nucleus.edges[i,2]] - nucleus.x[nucleus.edges[i,1]],
-                  nucleus.y[nucleus.edges[i,2]] - nucleus.y[nucleus.edges[i,1]],
-                  nucleus.z[nucleus.edges[i,2]] - nucleus.z[nucleus.edges[i,1]]];
+nuc.z .+= 0
+
+for i = 1:size(nuc.edges,1)
+
+        vector = [nuc.x[nuc.edges[i,2]] - nuc.x[nuc.edges[i,1]],
+                  nuc.y[nuc.edges[i,2]] - nuc.y[nuc.edges[i,1]],
+                  nuc.z[nuc.edges[i,2]] - nuc.z[nuc.edges[i,1]]];
                       
-        lengths[j] = norm(vector);
-        j += 1;
-    end
+        lengths[i] = norm(vector);
+
 end
-nucleus.normalLength = mean(lengths);
+nuc.normalLengths = lengths;
 
-frictionMatrix = get_friction_matrix(nucleus);
+frictionMatrix = get_friction_matrix(nuc,par);
 
-#println("the volume of the sphere is $nucleus.normalVolume")
-#println("the area of the sphere is $nucleus.normalArea")
+#println("the volume of the sphere is $nuc.normalVolume")
+#println("the area of the sphere is $nuc.normalArea")
 
-for t = 1:100
+maxT = 666;
+
+p = Progress(maxT)
+anim = Animation();
+
+# trace = PlotlyJS.mesh3d(
+#     x=nuc.x,
+#     y=nuc.z,
+#     z=nuc.y,
+#     i=nuc.tri[:,1] .- 1,
+#     j=nuc.tri[:,2] .- 1,
+#     k=nuc.tri[:,3] .- 1
+# );
+# data = [trace];
+# pl = PlotlyJS.plot(data,layout)
+
+for t = 1:maxT
     
-    get_voronoi_areas!(nucleus);
-    get_local_curvatures!(nucleus);
-    get_area_unit_vectors!(nucleus);
-    get_triangle_normals!(nucleus);
+    get_voronoi_areas!(nuc);
+    get_local_curvatures!(nuc);
+    get_area_unit_vectors!(nuc);
+    get_triangle_normals!(nuc);
 
-    forces.volume = get_volume_forces(nucleus);
-    forces.area = get_area_forces(nucleus);
-    forces.bending = get_bending_forces(nucleus);
-    forces.elastic = get_elastic_forces(nucleus);
-    forces.repulsion = get_repulsion_forces(nucleus);
+    get_volume_forces!(nuc,par);
+    get_area_forces!(nuc,par);
+    get_bending_forces!(nuc,par);
+    get_elastic_forces!(nuc,par);
+    get_repulsion_forces!(nuc,par);
 
+    flatRepulsion = flat_repulsion_forces(nuc);
+    if t >= 0
+        flatRepulsion2 = flat_repulsion_forces2(nuc,t);
+    else
+        flatRepulsion2 = zeros(Float64,length(nuc.x),3);
+    end
+
+    local totalForcesX = nuc.forces.volumeX .+ nuc.forces.areaX .+ nuc.forces.bendingX .+ nuc.forces.elasticX .+ nuc.forces.repulsionX;
+    local totalForcesY = nuc.forces.volumeY .+ nuc.forces.areaY .+ nuc.forces.bendingY .+ nuc.forces.elasticY .+ nuc.forces.repulsionY;
+    local totalForcesZ = nuc.forces.volumeZ .+ nuc.forces.areaZ .+ nuc.forces.bendingZ .+ nuc.forces.elasticZ .+ nuc.forces.repulsionZ .+ flatRepulsion2[:,3];
+    
+    local vX = cg(frictionMatrix,totalForcesX);
+    local vY = cg(frictionMatrix,totalForcesY);
+    local vZ = cg(frictionMatrix,totalForcesZ);
+    
+    nuc.x = nuc.x .+ vX.*0.01;
+    nuc.y = nuc.y .+ vY.*0.01;
+    nuc.z = nuc.z .+ vZ.*0.01;
+
+    if mod(t,5) == 0
+        plot_sphere!(nuc,[vX vY vZ],t)
+        Plots.frame(anim)
+    end
+    next!(p)
 end
-end
 
-plot_sphere!(nucleus,forces.repulsion)
-
+gif(anim, "test.gif", fps = 30)
--- misc_functions.jl
+++ misc_functions.jl
@@ -1,7 +1,40 @@
-function get_friction_matrix(nucleus)
+function get_friction_matrix(nuc,par)
 
-    frictionMatrix = sparse([],[],[],3*length(nucleus.x),3*length(nucleus.x));
+    frictionMatrix = sparse(Int64[],Int64[],Float64[],length(nuc.x),length(nuc.x));
+
+    for j = 1:length(nuc.x)
+        frictionMatrix[j,j] = par.viscosity + par.laminaFriction*length(nuc.neighbors[j]);
+        frictionMatrix[j,nuc.neighbors[j]] .= -par.laminaFriction;
+    end
 
     return frictionMatrix
+
+end
+
+function read_parameters(filePath)
+
+    f = open(filePath)
+
+    inputParameters = Dict{String,Float64}()
+
+    while !eof(f)
+        line = split(readline(f), ',')
+        inputParameters[line[1]] = parse(Float64,line[2])
+    end
+
+    return inputParameters
+end
+
+function calculate_model_parameters(ipar)
+
+    mpar.laminaStiffness
+    laminaStiffness,
+areaCompressionStiffness,
+bendingStiffness,
+nucleusBulkModulus,
+repulsionConstant,
+viscosity,
+laminaFriction,
+    return mpar
 
 end
